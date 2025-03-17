aa3af30de77e288c9ad49527b1f95ba7676517b5
Talau08 <160486729+juliatalau@users.noreply.github.com>
2025-03-17 07:42:01 -0300

    ..

--- script.js
+++ script.js
@@ -18,3 +18,6 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
 })
+
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
